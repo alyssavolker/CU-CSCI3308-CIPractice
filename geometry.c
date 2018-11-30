c0f96b36260d357d1688e67fb8d51d96ef778bfb
Alyssa Volker <alyssavolker@engr2-2-90-189-dhcp.int.colorado.edu>
2018-11-30 15:15:51 -0700

    Part 2

--- geometry.c
+++ geometry.c
@@ -1,6 +1,6 @@
 /*
  * geometry.c
- * Andy Sayler
+ * Alyssa Volker
  * CSCI 3308
  * Summer 2014
  *
@@ -67,5 +67,10 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     /* Maths */
     mid->x = ((a->x + b->x) / 2.0 );
     mid->y = ((a->y + b->y) / 2.0 );
+
+}
+double coord_2d_area_triangle(const coord_2d_t* a, const
+coord_2d_t* b, const coord_2d_t* c){
+    return (a->x*(b->y -c->y)+b->x*(c->y-a->y)+c->x*(a->y-b->y))/2;
 
 }
